3052bf9fac6ee6ecf879bc0a93204f5491c3bf02
rizwanhkm <rizwan.hkm@gmail.com>
2015-09-14 21:31:43 +0530

    links fixed

--- sendemail.php
+++ sendemail.php
@@ -1,21 +1,21 @@
 <?php
-header('Content-type: application/json');
-$status = array(
-    'type'=>'success',
-    'message'=>'Email sent!'
-);
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Email sent!'
+	);
 
-$name = @trim(stripslashes($_POST['name'])); 
-$email = @trim(stripslashes($_POST['email'])); 
-$subject = @trim(stripslashes($_POST['subject'])); 
-$message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+    $subject = @trim(stripslashes($_POST['subject'])); 
+    $message = @trim(stripslashes($_POST['message'])); 
 
-$email_from = $email;
-$email_to = '111113070@nitt.edu';
+    $email_from = $email;
+    $email_to = '111113070@nitt.edu';
 
-$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
-$success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
-echo json_encode($status);
-die;
+    echo json_encode($status);
+    die;
